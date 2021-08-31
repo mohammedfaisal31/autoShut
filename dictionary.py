441bbaf931d2e8907058fb62a842371d506158f6
Mohammed Soharab S <soharab@quinta.co.in>
2021-08-31 20:20:21 +0530

    dictionary app

---
+++ dictionary.py
@@ -0,0 +1,42 @@
+import colorama
+from colorama import Fore
+import requests as req
+import json as j
+import pyperclip as p
+from tkinter import Tk  
+prev = ''
+
+def getFormatString(str):
+	return "| "+str+" "*(99-len(str))+"|"
+
+
+def getDictionary(word):
+		try:
+			print("******** word: "+word+"********")
+			res = req.get("https://api.dictionaryapi.dev/api/v2/entries/en/"+word)
+			meanings_list = res.json()[0]['meanings']
+			for i in range(len(meanings_list)):
+				print("+"+"-"*100+"+")
+				print(getFormatString("Definition: "+ str(i+1)))
+				try:
+					def_obj = meanings_list[i]['definitions'][0]
+					print(getFormatString("definition: ****  "+def_obj['definition']+"  ****"))
+					print(getFormatString("Parts of speech: "+meanings_list[i]['partOfSpeech']+""))
+					print(getFormatString("example: "+def_obj['example']))
+				except:
+					print("|   Something went wrong!")
+			print("+"+"-"*100+"+\n")	
+			print(res)
+		except : 
+			print("No response")
+
+
+while(True):
+	try:
+		word = Tk().clipboard_get()
+		if(word != '' and word != prev):
+			getDictionary(word)
+		prev = word
+	except:
+		print("ERROR!")
+		
